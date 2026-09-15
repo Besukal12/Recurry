906e4222829aa2f58787d635df73323334e11700
Besukal gobena <besukalgobena@gmail.com>
2026-09-15 21:36:25 +0300

    Fix import paths and update main entry in package.json; add index.ts for app registration

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { ScreenContent } from '../components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
-import './global.css';
+import '../global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
