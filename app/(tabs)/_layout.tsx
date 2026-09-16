05042d922aaf7cd6f871ea540da416eb7ca2006e
Besukal gobena <besukalgobena@gmail.com>
2026-09-16 23:58:36 +0300

    feat: add constants for tabs and icons, and declare image modules

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Tabs } from 'expo-router';
+import { tabs } from 'constants/data';
+import { View, Image } from 'react-native';
+import clsx from 'clsx';
+
+const TabLayout = () => {
+  const TabIcons = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx('tabs-pill', focused && 'tabs-active')}>
+          <Image source={icon} className="tabs-glyph" />
+        </View>
+      </View>
+    );
+  };
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => <TabIcons focused={focused} icon={tab.icon} />,
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+};
+
+export default TabLayout;
