19407bbf4cdcaf4e208612db2af99e7c748a303f
Besukal gobena <besukalgobena@gmail.com>
2026-09-17 01:03:28 +0300

    feat: add custom fonts and update layout styles for improved UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
       <View className="mb-6 w-full px-2">
-        <Text className="text-center text-2xl font-black text-slate-800">This is Home page</Text>
+        <Text className="text-center text-2xl text-primary font-sans-extrabold">This is Home page</Text>
         <Text className="mt-1 text-center text-sm font-medium text-slate-500">
           Explore your personalized system metrics
         </Text>
@@ -18,21 +18,10 @@ export default function App() {
         <View className="mt-4 flex flex-row items-center justify-center gap-4">
           <Link
             href="./(auth)/sign-up"
-            className="bg-success rounded-2xl border-1 p-3 text-amber-50">
+            className="bg-primary rounded-2xl border-1 p-3 text-white">
             Sign up
           </Link>
           <Link href="./(auth)/sign-in">Sign in</Link>
-        </View>
-
-        <View className="mt-5 flex w-full items-center justify-center gap-4">
-          <Link href="/subscription/spotify">spotify subscription</Link>
-          <Link
-            href={{
-              pathname: '/subscriptions/[id]',
-              params: { id: 'claude' },
-            }}>
-            claude subscription
-          </Link>
         </View>
       </View>
     </SafeAreaView>
